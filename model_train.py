dcfad13267575f88a18999d7958991152e481c9c
S0lonka <solowayplay@gmail.com>
2025-07-13 02:46:20 +0300

    добавил комент и пару измененний

--- model_train.py
+++ model_train.py
@@ -2,12 +2,19 @@ from ultralytics import YOLO
 
 from config import yolo_model
 
+# ! ЗАПУСКАТЬ ЧЕРЕЗ КОНСОЛЬ - Python НЕ ВИДИТ Видео карту
 
-model = YOLO(f"yolo_model\\{yolo_model}")
 
-model.train(
+model = YOLO(f"yolo_model\\{yolo_model}").to("cuda")
+
+results = model.train(
     data = "data_set_config.yaml",
     epochs=400,
+    imgsz=640,
+    batch=16,        # Можно уменьшить, если не хватает памяти GPU
     device="cuda",
     name=f"{yolo_model}_train"
 )
+
+results = model.predict("data_set\\test\\images\\enemy_148_png.rf.6e716582bc98c8e4056deadf89a4c79f.jpg", conf=0.5)
+results[0].show()  # Показать результат
