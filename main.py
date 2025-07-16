4d69316370293ae5bfa1eec2763d4f479c529bc0
S0lonka <solowayplay@gmail.com>
2025-07-16 21:39:09 +0300

    Заготовка для main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from app.utils.model.model_train import train_model, resume_train_model
 
 
+
+def main():
+    pass
+
+
+
 if __name__ == "__main__":
-    resume_train_model(model_path=r"AI\runs\standoff_yolo11m_V\weights\last.pt")
+    main()
