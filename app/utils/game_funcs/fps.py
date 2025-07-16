92ad5fd1edbbe73abf1780e8732ffd754e3d2aea
S0lonka <solowayplay@gmail.com>
2025-07-17 00:15:48 +0300

    рабочий main

--- app/utils/game_funcs/fps.py
+++ app/utils/game_funcs/fps.py
@@ -10,10 +10,10 @@ def display_fps(frame: numpy.ndarray, fps: int):
     '''
     cv2.putText(
         frame,                       # Изображение(куда)
-        f"FPS: {int(fps)}",          # Текст
-        (30, 50),                    # Позиция
+        f"{int(fps)}",               # Текст
+        (20, 50),                    # Позиция
         cv2.FONT_HERSHEY_SIMPLEX,    # Стиль
-        2,                           # Размер
+        1.5,                           # Размер
         (0, 255, 0),                 # Цвет
         2                            # Толщина
     )
--- main.py
+++ main.py
@@ -12,6 +12,10 @@ from app.utils.game_funcs.fps import display_fps
 def main(model) -> None:
     cap = cv2.VideoCapture(OBS_CAPTURE) # Захватываем виртуальную камеру OBS
 
+    #----Можно изменить размер изображения, НО жертвуя FPS----
+    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
+
     #-----FPS-----
     prev_time = 0
     fps = 0
@@ -33,13 +37,16 @@ def main(model) -> None:
         results = model(
             frame,                      # Изображение над которым работаем
             imgsz=640,                  # Размер обработки (меньше = быстрее)
-            half=True,                  # Половинная точность (если GPU с CUDA)
+            half=True,                  # Половинная точность (если GPU с CUDA) | +FPS
             device='cuda',              # Явно указываем GPU
             conf=CONFIDENCE_THRESHOLD,  # Порог уверенности (меньше = быстрее, но менее точно)
-            verbose=False               # verbose=False отключает лишний вывод
+            verbose=False,              # verbose=False отключает лишний вывод
+            # iou=0.45                    # Порог для подавления дубликатов
         )
 
         annotated_frame = results[0].plot()  # Кадр с нарисованными Боксами
+
+
 
 
         #----------Расчитываем FPS----------
@@ -47,11 +54,20 @@ def main(model) -> None:
         fps = 1 / (current_time - prev_time) if (current_time - prev_time) != 0 else 0
         prev_time = current_time
 
-
         #----------Отображаем FPS-----------
         display_fps(annotated_frame, int(fps))
 
 
+
+        #-----Отрисовываем окно-----
+        cv2.imshow("Yolo + OBS boxs", annotated_frame)
+
+        #-----Обработка завершения программы----- (без этого окно зависнет)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 
